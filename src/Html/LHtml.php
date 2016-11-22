a2e952e2d3a1134f0067bd21852a704b5af4f093
Alexey Berdnikov <berdnikov_ay@bunkermedia.ru>
2016-11-22 13:41:46 +0500

    fixed

--- src/Html/LHtml.php
+++ src/Html/LHtml.php
@@ -2,74 +2,74 @@
 
 namespace Larakit\Html;
 
-class LHtml extends \HTML_Common2
-{
+class LHtml extends \HTML_Common2 {
+
     protected $_content    = '';
     protected $_type       = '';
     protected $_has_closed = true;
+    protected $attributes  = [
+        'lang' => 'ru',
+    ];
 
-    function setValue($value)
-    {
+    function setValue($value) {
         $this->setAttribute('value', $value);
+
         return $this;
     }
 
-    function ngApp($value='larakit'){
+    function ngApp($value = 'larakit') {
         $this->setAttribute('ng-app', $value);
+
         return $this;
     }
 
-    function setId($value)
-    {
+    function setId($value) {
         $this->setAttribute('id', $value);
+
         return $this;
     }
 
-    function setTitle($value)
-    {
+    function setTitle($value) {
         $this->setAttribute('title', $value);
+
         return $this;
     }
 
-    function getTitle()
-    {
+    function getTitle() {
         return $this->getAttribute('title');
     }
 
-    function setContent($content)
-    {
+    function setContent($content) {
         $this->_content = $content;
+
         return $this;
     }
 
-    function getType()
-    {
+    function getType() {
         return $this->_type;
     }
 
-    function setType($type)
-    {
+    function setType($type) {
         $this->_type = $type;
+
         return $this;
     }
 
-    function setHasClosed($val)
-    {
-        $this->_has_closed = (bool)$val;
+    function setHasClosed($val) {
+        $this->_has_closed = (bool) $val;
+
         return $this;
     }
 
-    function __toString()
-    {
-        if ($this->_has_closed) {
+    function __toString() {
+        if($this->_has_closed) {
             return sprintf('<%s%s>%s</%s>', $this->_type, $this->getAttributes(true), $this->getContent(), $this->_type);
         } else {
             return sprintf('<%s%s />', $this->_type, $this->getAttributes(true));
         }
     }
 
-    function getContent()
-    {
+    function getContent() {
         return $this->_content;
     }
 
